0fe87996a9a40b10fa6b67cd2a6156ec1eb23e38
gameboyfun <50244142+gameboyfun@users.noreply.github.com>
2019-05-09 21:30:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ $bot = new Linebot();
 //$bot->reply($text);
 
 $imageUrl = "https://sites.google.com/site/khxmuleleasarsnthes/_/rsrc/1480953778509/watthuprasngkh/googlelogo_color_284x96dp.png";
-$bot->pushImage($userId, $imageUrl);
+$bot->pushImage($imageUrl);
