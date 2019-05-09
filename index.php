55c06826b2cac7c3cd06c8f40daff4ff59f91830
gameboyfun <50244142+gameboyfun@users.noreply.github.com>
2019-05-09 21:33:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $bot = new Linebot();
 //$text = $bot->getMessageText();
 //$bot->reply($text);
 
-$imageUrl = "https://sites.google.com/site/khxmuleleasarsnthes/_/rsrc/1480953778509/watthuprasngkh/googlelogo_color_284x96dp.png";
-$bot->pushImage($imageUrl);
+$imageUrl = "https://ichef.bbci.co.uk/news/976/cpsprodpb/27C9/production/_103158101_tha.jpg";
+$bot->pushImage($userId,$imageUrl);
