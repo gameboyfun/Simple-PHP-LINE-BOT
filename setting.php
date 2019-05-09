ef0ddfcafeb36bc065587b56ad26ffb8f832e6d1
gameboyfun <50244142+gameboyfun@users.noreply.github.com>
2019-05-09 21:05:12 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 
 class Setting {
 	public static function getChannelAccessToken(){
-		$channelAccessToken = "";
+		$channelAccessToken = "6jtTR4BgR5uI86oUoxODYNUyTKM0UeziXIQI97uPhtk8tIJtisAk2lsP5Xs6F7rToTr3XyfTRdmTicsShZu8sxRhWQ6Cin/TXX/2J8H429AB/wDw0XcSlIrOK5CuC1c7TDUxW5uiUERSbig1ebUaswdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public static function getChannelSecret(){
-		$channelSecret = "";
+		$channelSecret = "6c8f950137a781d397c41e9cdefff164";
 		return $channelSecret;
 	}
 	public static function getApiReply(){
